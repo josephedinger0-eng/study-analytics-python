f899e30a1f2bdf179627b3f063157125ceaece1f
Joseph Edinger <josephedinger0@gmail.com>
2026-09-04 09:02:48 -0600

    Create basic study session input

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+subject = input("Enter your subject: ")
+minutes = int(input("Enter the number of minutes: "))
+score = int(input("Enter your score: "))
+
+print(f"Hello {name}, you have spent {minutes} minutes on {subject} and scored {score}.")
